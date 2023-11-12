b05a29d1b75029342a0758cbba2937a05c71e92f
renaissancenerd <vatsallimbachiya@gmail.com>
2023-11-12 13:45:24 +0100

    two sum in kt

--- src/TwoSum.kt
+++ src/TwoSum.kt
@@ -1,6 +1,3 @@
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
  *
@@ -16,23 +13,18 @@ import java.util.Map;
  * Output: [0,1]
  * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
  */
-
-
-
-public class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int[] result = new int[2];
-        for(int i=0; i<nums.length; i++){
-            if(map.containsKey(target - nums[i])){
-                result[1] = i;
-                result[0]= map.get(target - nums[i]);
-                return result;
-
+class TwoSum {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val map: MutableMap<Int, Int> = HashMap()
+        val result = IntArray(2)
+        for (i in nums.indices) {
+            if (map.containsKey(target - nums[i])) {
+                result[1] = i
+                result[0] = map[target - nums[i]]!!
+                return result
             }
-            map.put(nums[i], i);
-
+            map[nums[i]] = i
         }
-        return result;
+        return result
     }
 }
